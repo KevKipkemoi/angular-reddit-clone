03cd3f122995e9190bd49da5f30eb412e7ff5480
KevKipkemoi <kevkipkemoi@outlook.com>
2020-10-24 20:56:49 +0300

    signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { SignUpComponent } from './auth/sign-up/sign-up.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './signup-request.payload';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signupRequestPayload: SignupRequestPayload;
+  signupForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  signup() {
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload).subscribe(() => {
+      console.log('Signup Successful');
+    }, () => {
+      console.log('Signup Failed');
+    });
   }
 
 }
